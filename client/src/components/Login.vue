b506c17222de281bef56493239e946772d7bbe94
Félix Sabatié <felix.sabatie@gmail.com>
2018-04-06 08:45:29 +0200

    Init login component

---
+++ client/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <form v-on:submit.prevent="login">
+      <label for="name-input">Name :</label>
+      <input id="name-input" type="text" v-model="name">
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: 'login',
+    computed: {
+      ...mapGetters(['socket'])
+    },
+    data() {
+      return {
+        name: ''
+      }
+    },
+    methods: {
+      login() {
+        this.io.emit('login', this.name)
+      }
+    }
+  }
+</script>
